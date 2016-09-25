e262a649ce1fbf7fb4f047600bac251e70594500
ThiagoSantana <thiagorodriguescamra@gmail.com>
2016-09-25 16:02:54 -0300

    persiatencia e negocio Usuario

---
+++ Kanban/src/negocio/NUsuario.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package negocio;
+
+import entidade.EUsuario;
+import java.sql.SQLException;
+import java.util.List;
+import persistencia.PUsuario;
+
+public class NUsuario {
+
+    PUsuario pUsuario = new PUsuario();
+    EUsuario eUsuario;
+
+    public void inserir(EUsuario eUsuario) throws Exception {
+        if (eUsuario.getNome().isEmpty() || eUsuario.getEmail().isEmpty()) {
+            throw new Exception("O Usuário deve conter pelo menos um nome e um email para cadastro!");
+        }
+
+        pUsuario.Inserir(eUsuario);
+    }
+
+    public EUsuario Buscar(int codigo) throws Exception{
+        if(codigo==0)
+            throw new Exception("Informe um codigo para busca!");
+        
+        return pUsuario.Buscar(codigo);
+    }
+    public EUsuario Buscar(String nome) throws Exception{
+        
+        return pUsuario.Buscar(nome);
+    }
+    public void Excluir(int codigo, String senha, String email) throws Exception{
+        
+        
+        if(!ValidarLogin(senha,email))
+            throw new Exception("Você não tem permissão para excluir esse usuário!");
+        
+        pUsuario.Excluir(eUsuario.getCodigo());
+        
+    }
+    public List<EUsuario> Listar() throws SQLException{
+        
+        return pUsuario.Listar();
+    }
+
+    private boolean ValidarLogin(String email, String senha) throws SQLException, Exception {
+        
+        if(!pUsuario.BuscarPorEamil(email).getSenha().equals(senha))
+             throw new Exception("Usuário não encontrado!");
+             
+        
+          eUsuario = pUsuario.BuscarPorEamil(email);
+          return true;
+        
+    }
+    
+
+}
---
+++ Kanban/src/persistencia/PUsuario.java
@@ -0,0 +1,124 @@
+package persistencia;
+
+import entidade.EUsuario;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+public class PUsuario {
+
+    public void Inserir(EUsuario eUsuario) throws SQLException {
+
+        String sql = "INSERT INTO public.usuario(senha, email, nome)\n"
+                + "VALUES ('" + eUsuario.getSenha() + "','" + eUsuario.getEmail() + "','" + eUsuario.getNome() + "');";
+
+            Connection conn = util.Conexao.getConexao();
+            Statement prd = conn.createStatement();
+            prd.execute(sql);
+            conn.close();
+       
+    }
+
+    public void Alterar(EUsuario eUsuario) throws SQLException {
+        String sql = "UPDATE public.usuario SET senha='" + eUsuario.getSenha() + "',"
+                + " email='" + eUsuario.getEmail() + "', nome='" + eUsuario.getNome() + "'WHERE codigo = " + eUsuario.getCodigo() + ";";
+
+        Connection conn = util.Conexao.getConexao();
+        Statement prd = conn.createStatement();
+        prd.execute(sql);
+        conn.close();
+    }
+
+    public void Excluir(int codigo) throws SQLException {
+        String sql = "DELETE FROM public.usuario WHERE codigo = " + codigo + ";";
+
+        try (Connection conn = util.Conexao.getConexao()) {
+            Statement st = conn.createStatement();
+            st.executeUpdate(sql);
+        }
+    }
+
+    public List<EUsuario> Listar() throws SQLException {
+
+        String sql = "SELECT codigo, senha, email, nome FROM public.usuario;";
+
+        Connection conn = util.Conexao.getConexao();
+
+        Statement st = conn.createStatement();
+        ResultSet rs = st.executeQuery(sql);
+        List<EUsuario> list = new ArrayList<>();
+
+        while (rs.next()) {
+
+            EUsuario eUsuario = new EUsuario();
+
+            eUsuario.setCodigo(rs.getInt("codigo"));
+            eUsuario.setSenha(rs.getString("senha"));
+            eUsuario.setSenha(rs.getString("email"));
+            eUsuario.setSenha(rs.getString("nome"));
+
+            list.add(eUsuario);
+
+        }
+        return list;
+
+    }
+
+    public EUsuario Buscar(int codigo) throws SQLException {
+        String sql = "SELECT codigo, senha, email, nome FROM public.usuario where codigo = " + codigo + ";";
+
+        Connection conn = util.Conexao.getConexao();
+
+        Statement st = conn.createStatement();
+        ResultSet rs = st.executeQuery(sql);
+
+        EUsuario eUsuario = new EUsuario();
+
+        eUsuario.setCodigo(rs.getInt("codigo"));
+        eUsuario.setSenha(rs.getString("senha"));
+        eUsuario.setSenha(rs.getString("email"));
+        eUsuario.setSenha(rs.getString("nome"));
+
+        return eUsuario;
+    }
+
+    public EUsuario Buscar(String nome) throws SQLException {
+        String sql = "SELECT codigo, senha, email, nome FROM public.usuario where nome like = '" + nome + "'%;";
+
+        Connection conn = util.Conexao.getConexao();
+
+        Statement st = conn.createStatement();
+        ResultSet rs = st.executeQuery(sql);
+
+        EUsuario eUsuario = new EUsuario();
+
+        eUsuario.setCodigo(rs.getInt("codigo"));
+        eUsuario.setSenha(rs.getString("senha"));
+        eUsuario.setSenha(rs.getString("email"));
+        eUsuario.setSenha(rs.getString("nome"));
+
+        return eUsuario;
+    }
+    
+      public EUsuario BuscarPorEamil(String email) throws SQLException {
+        String sql = "SELECT codigo, senha, email, nome FROM public.usuario where email like = '" + email + "'%;";
+
+        Connection conn = util.Conexao.getConexao();
+
+        Statement st = conn.createStatement();
+        ResultSet rs = st.executeQuery(sql);
+
+        EUsuario eUsuario = new EUsuario();
+
+        eUsuario.setCodigo(rs.getInt("codigo"));
+        eUsuario.setSenha(rs.getString("senha"));
+        eUsuario.setSenha(rs.getString("email"));
+        eUsuario.setSenha(rs.getString("nome"));
+
+        return eUsuario;
+    }
+
+}
